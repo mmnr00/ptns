3b9a21a6920789b2ae350056a22f12a124c891ff
mmnr00 <mmnr00@gmail.com>
2021-01-17 11:24:26 +0800

    tsk

--- app/controllers/ptns_mmbs_controller.rb
+++ app/controllers/ptns_mmbs_controller.rb
@@ -19,6 +19,21 @@ class PtnsMmbsController < ApplicationController
 			  standalone: true
 			)
 
+			png = qrcode.as_png(
+			  bit_depth: 1,
+			  border_modules: 4,
+			  color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+			  color: 'black',
+			  file: nil,
+			  fill: 'white',
+			  module_px_size: 6,
+			  resize_exactly_to: false,
+			  resize_gte_to: false,
+			  size: 200
+			)
+
+			IO.binwrite("app/assets/images/qrcodes/qr_#{params[:id]}.png", png.to_s)
+
 
 			render action: "checkmmb", layout: "eip"
 		else
